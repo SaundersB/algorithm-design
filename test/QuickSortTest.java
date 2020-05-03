90a872083178fc2d8721fd810d5e15301d8351c4
Brandon Saunders <BrandonNSaunders@gmail.com>
2020-05-02 18:54:58 -0700

    Adding another test for QuickSort with negative numbers.

--- test/QuickSortTest.java
+++ test/QuickSortTest.java
@@ -10,4 +10,12 @@ public class QuickSortTest {
         int[] result = QuickSort.quickSort(testArray, 0, 6);
         assertArrayEquals(expectedResult, result);
     }
+
+    @Test
+    public void testQuickSortWithNegativeNumbers(){
+        int[] testArray = {4, -1193939, 6, -14, 1, 2, 49823, 3};
+        int[] expectedResult = {-1193939, -14, 1, 2, 3, 4, 6, 49823};
+        int[] result = QuickSort.quickSort(testArray, 0, 7);
+        assertArrayEquals(expectedResult, result);
+    }
 }
